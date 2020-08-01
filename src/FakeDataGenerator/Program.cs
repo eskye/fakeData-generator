96be8ebcb45ff875ce647ef47231bc767137e14b
eskye <sunkanmiijatuyi@gmail.com>
2020-08-01 10:52:59 +0100

    initial commit

---
+++ src/FakeDataGenerator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Bogus;
+
+namespace FakeDataGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var billingDetailsFaker = new Faker<BillingDetails>()
+                .RuleFor(x => x.CustomerName, x=>x.Person.FullName)
+                .RuleFor(x => x.Email, x => x.Person.Email)
+                .RuleFor(x => x.AddressLine, x => x.Address.StreetAddress())
+                .RuleFor(x => x.City, x => x.Address.City())
+                .RuleFor(x => x.Country, x => x.Address.Country())
+                .RuleFor(x => x.Phone, x => x.Person.Phone)
+                .RuleFor(x => x.PostCode, x => x.Address.ZipCode());
+
+            var text = JsonSerializer.Serialize(billingDetailsFaker.Generate());
+
+            Console.WriteLine(text);
+        }
+    }
+}
